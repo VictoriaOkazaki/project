a7b3b327e65d10c3a694022c1a745ef755fa9c44
VictoriaOkazaki <victoria.timein@gmail.com>
2023-12-03 19:30:08 +0400

    random imgs & texts

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
+const texts = {
+    '1.jpg': 'Sweet Bonanza1',
+    '2.jpg': 'Sweet Bonanza2',
+    '3.jpg': 'Sweet Bonanza3',
+    '4.jpg': 'Sweet Bonanza4'
+}
+
+const imgs = Object.keys(texts)
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+function getTwoRandomImgs() {
+    const index1 = getRandomInt(0, imgs.length - 1)
+    let index2 = getRandomInt(0, imgs.length - 1)
+    if (index1 === index2) {
+        index2 = (index2 + 1) % imgs.length
+    }
+    const img1 = imgs[index1]
+    const img2 = imgs[index2]
+    const text1 = texts[img1]
+    const text2 = texts[img2]
+
+    const res1 = { src: `img/${img1}`, text: text1 }
+    const res2 = { src: `img/${img2}`, text: text2 }
+
+    return [res1, res2]
+}
+
 //fake buttons click only one
 const btnsTip = document.querySelectorAll(".fake-btn");
 let activeBtn = null;
@@ -27,8 +59,11 @@ progressContainers.forEach((containerEl, index) => {
         const twoBarsDone = progresessList[0] && progresessList[1]
 
         if (twoBarsDone) {
-            document.getElementById('btn1').innerHTML = 'Book of Ra'
-            document.getElementById('btn2').innerHTML = 'Sweet Bonanza'
+            const [btn1, btn2] = getTwoRandomImgs()
+            document.getElementById('btn1').innerHTML = btn1.text
+            document.getElementById('btn2').innerHTML = btn2.text
+            document.getElementById('img-1').src = btn1.src
+            document.getElementById('img-2').src = btn2.src
 
             document.querySelectorAll('.btn-img').forEach(element => element.classList.add('visible'))
         }
